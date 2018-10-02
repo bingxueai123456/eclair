3fb2a9d7dffda7573eadd8baceeddc19b5b0fab8
eclair <dongbc@silvrr.com>
2018-10-02 17:49:40 +0800

    增加AppConfig类

---
+++ src/main/java/com/ice/eclair/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ice.eclair.config;
+
+/**
+ * @Auther: eclair
+ * @Date: 2018/10/2 14:48
+ * @Description:
+ */
+public class AppConfig {
+	public final static String DB_SLAVE = "slave";
+	public final static String DB_MASTER = "master";
+}
